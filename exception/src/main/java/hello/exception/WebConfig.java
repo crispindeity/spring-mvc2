5092da1d40459c7f64b17828cf4e4d06347bc453
geombong <shoy1415@gmail.com>
2022-05-09 22:10:56 +0900

    오류 페이지와 예외처리

---
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,34 @@
+package hello.exception;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+
+import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+//    @Bean
+    public FilterRegistrationBean<Filter> logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+//        filterFilterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST);
+
+        return filterFilterRegistrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");
+    }
+}
