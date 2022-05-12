e39e4281c6adef8a6f55ccc69bc6d563bdf90b46
geombong <shoy1415@gmail.com>
2022-05-13 03:08:31 +0900

    formatter

--- typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,19 +4,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
-import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formater.MyNumberFormatter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new IpPortToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
